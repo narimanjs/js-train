6e8f7aa643667f4bb6ab097e079b746eb738e7e9
Nariman <mirmanov1@mail.ru>
2022-08-05 21:10:38 +0300

    feat: add gh-pages

--- index.js
+++ index.js
@@ -13,9 +13,80 @@
 // console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 // console.log(document.querySelector('[data-current="3"]').previousElementSibling);
 
-for (let node of document.body.childNodes) {
-    if(node.nodeName == "#text") {
-        continue;
+// for (let node of document.body.childNodes) {
+//     if(node.nodeName == "#text") {
+//         continue;
+//     }
+//     console.log(node);
+// }
+
+// function pow (x, y) {
+//     let result = 1;
+
+//     for(let i = 0; i < y; i++) {
+//         result *= x;
+//         return result;
+//     }
+// }
+
+// function pow (x, y) {
+//     if(y === 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, y - 1);
+//     }
+// }
+// pow (2, 1) // 2
+// pow (2, 2) // 4
+// pow (2, 3) // 8
+// pow (2, 4) // 16
+
+let students = {
+    js: [{
+        name: 'john',
+        progress: 10
+    },
+    {
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        },
+        {
+            name: 'Ann',
+            progress: 18
+        }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
     }
-    console.log(node);
+    
+};
+function getTotalProgressByIteration (data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if(Array.isArray(course)) {
+            students += course.length;
+            for(let i = 0; i < course.length; i++) {
+                total += total + course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)){
+                students += subCourse.length;
+                
+                for(let i = 0; i < subCourse.length; i++) {
+                    total += total + subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
 }
+console.log(getTotalProgressByIteration(students));
